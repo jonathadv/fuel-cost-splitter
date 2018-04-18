33edf6c7662e74d7552ae067aea4f2b4270021b3
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2018-04-18 07:58:00 -0300

    Saves working state for GooglePlaceAutocomplete.js

--- src/integration/GooglePlaceAutocomplete.js
+++ src/integration/GooglePlaceAutocomplete.js
@@ -5,21 +5,13 @@ class GooglePlacesAutocomplete extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initLat: this.props.initLat,
-      initLng: this.props.initLng,
     };
   }
 
+  // Based on Google Places Autocomplition documentation
+  // https://developers.google.com/maps/documentation/javascript/places-autocomplete 
   setGoogleObjectsToWindow() {
     let placeSearch, autocomplete;
-    const componentForm = {
-      street_number: 'short_name',
-      route: 'long_name',
-      locality: 'long_name',
-      administrative_area_level_1: 'short_name',
-      country: 'long_name',
-      postal_code: 'short_name',
-    };
 
     window.initAutocomplete = () => {
       // Create the autocomplete object, restricting the search to geographical
@@ -37,59 +29,35 @@ class GooglePlacesAutocomplete extends Component {
     window.fillInAddress = () => {
       // Get the place details from the autocomplete object.
       var place = autocomplete.getPlace();
+      console.log('######### Chose address is: ', place.formatted_address)
 
-      for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-      }
-
-      // Get each component of the address from the place details
-      // and fill the corresponding field on the form.
-      for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-          var val = place.address_components[i][componentForm[addressType]];
-          document.getElementById(addressType).value = val;
-        }
-      }
     };
 
-    // Bias the autocomplete object to the user's geographical location,
-    // as supplied by the browser's 'navigator.geolocation' object.
-    window.geolocate = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var geolocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          var circle = new window.google.maps.Circle({
-            center: geolocation,
-            radius: position.coords.accuracy,
-          });
-          autocomplete.setBounds(circle.getBounds());
-        });
-      }
-    };
+  }
+
+  onLoad() {
+    if (this.props.onLoad) {
+      this.props.onLoad();
+    }
   }
 
   loadApiLibrary() {
     const apiKey = process.env.REACT_APP_API_KEY;
     const script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key={apiKey}&callback=initMap'.replace(
+    script.src = 'https://maps.googleapis.com/maps/api/js?key={apiKey}&libraries=places&callback=initAutocomplete'.replace(
       '{apiKey}',
       apiKey
     );
     script.onload = () => {
-      this.onLoad();
+        this.onLoad();
     };
 
     this.setGoogleObjectsToWindow();
     document.body.appendChild(script);
   }
 
-  componentDidMount() {
-    this.loadApiLibrary.bind(this);
+  componentWillMount() {
+      return this.loadApiLibrary();
   }
 
   render() {
@@ -98,7 +66,6 @@ class GooglePlacesAutocomplete extends Component {
         <input
           id="autocomplete"
           placeholder="Enter your address"
-          onFocus={window.geolocate()}
           type="text"
         />
       </div>
@@ -107,14 +74,10 @@ class GooglePlacesAutocomplete extends Component {
 }
 
 GooglePlacesAutocomplete.propTypes = {
-  initLat: PropTypes.number,
-  initLng: PropTypes.number,
   onLoad: PropTypes.func,
 };
 
 GooglePlacesAutocomplete.defaultProps = {
-  initLat: -30.0277,
-  initLng: -51.2287,
 };
 
 export default GooglePlacesAutocomplete;
